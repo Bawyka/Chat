50e3911f4524efdbfb6a5f710d2ed8451f9565b9
bawyka <bawyka@gmail.com>
2013-09-03 15:15:31 +0300

    live chat

---
+++ post.php
@@ -0,0 +1,13 @@
+<?php session_start();
+
+if (isset($_SESSION['name']))
+{
+	$text = $_POST['text'];
+	
+	$fp = fopen("log.html",'a');
+	fwrite( $fp, "<div class='msgIn'>
+	(". date("g:i A"). ") <b>". $_SESSION['name']. "</b>: ". stripslashes(htmlspecialchars($text)) ."<br /></div>");
+	
+	fclose($fp);
+}
+?>
